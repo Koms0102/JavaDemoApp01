6d21fc4d23f0cf9a7e447ff4c038c77eeeec3b4b
Koms <kome.chinnusamy@mckesson.com>
2020-01-27 19:24:40 -0500

    Removed unwanted snippet

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -6,54 +6,6 @@ public class Application {
         System.out.println("hello World");
     }
 
-    boolean isActive(String status) {
-        // Implement your solution
-        boolean returnVal = false;
-        if (status.equals("active")) {
-            returnVal = true;
-        } else {
-            returnVal = false;
-        }
 
-        return returnVal;
-    }
-
-    String grade(int input) {
-        // Implement your solution
-
-        String gradeVal = " ";
-
-        if(input >= 90){
-            gradeVal="A";
-        }
-
-        else
-            if(input>=80){
-            gradeVal="B";
-        }
-        else
-            if(input>=70){
-            gradeVal="C";
-        }
-
-        else
-            if(input>=60){
-                gradeVal="D";
-
-        }
-
-        else gradeVal = "F";
-
-        return gradeVal;
-    }
-
-
-    public static void main(String[] args) {
-        int i=45;
-        while(i<=50){
-            System.out.println(i);
-            i++;
-        }
-    }
 
 }
